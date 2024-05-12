b130e2d8833da59a5984ee933c163bf86ec61612
IBRAHIM CELIK <168642694+SoftwareDEVibrahimcelik@users.noreply.github.com>
2024-05-12 13:21:16 +0300

    Add files via upload
    Download packages from nuget ! ;
    
    Cefsharp.Winforms
    Cefsharp.OffScreen
    
    
    
    ;
    CefSharp.Common.123.0.60
    CefSharp.OffScreen.123.0.60
    CefSharp.WinForms.123.0.60
    chromiumembeddedframework.runtime.win-x86.123.0.6

---
+++ ibrahimcelik - Game Collection for Steam/IBRAHIM CELIK - Game Collection for Steam/ibrahimcelik.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ibrahimcelik
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ChromiumWebBrowser1 = New CefSharp.WinForms.ChromiumWebBrowser()
+        Me.GroupBox1 = New Guna.UI2.WinForms.Guna2GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ElipsMain = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.DragControl1 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Elipse2 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Elipse3 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Elipse4 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Elipse5 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Elipse6 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ChromiumWebBrowser1
+        '
+        Me.ChromiumWebBrowser1.ActivateBrowserOnCreation = False
+        Me.ChromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ChromiumWebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.ChromiumWebBrowser1.Margin = New System.Windows.Forms.Padding(6)
+        Me.ChromiumWebBrowser1.Name = "ChromiumWebBrowser1"
+        Me.ChromiumWebBrowser1.Size = New System.Drawing.Size(2679, 1537)
+        Me.ChromiumWebBrowser1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Black
+        Me.GroupBox1.BorderColor = System.Drawing.Color.Red
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.CustomBorderColor = System.Drawing.Color.Black
+        Me.GroupBox1.FillColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(70, 63)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(750, 957)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.Text = "  İBRAHİM ÇELİK"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Black
+        Me.Button5.Location = New System.Drawing.Point(90, 766)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(574, 148)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Donation"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.Black
+        Me.Button4.Location = New System.Drawing.Point(90, 606)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(574, 148)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "ADD GAME - Testing"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(90, 446)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(574, 148)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "REMOVE ALL CARTS"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(90, 287)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(574, 148)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "REMOVE IF ALREADY EXITS"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(90, 127)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(574, 148)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "REMOVE IF NO DISCOUNT"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ElipsMain
+        '
+        Me.ElipsMain.BorderRadius = 50
+        '
+        'DragControl1
+        '
+        Me.DragControl1.DockIndicatorTransparencyValue = 0.6R
+        Me.DragControl1.UseTransparentDrag = True
+        '
+        'Elipse1
+        '
+        Me.Elipse1.BorderRadius = 100
+        Me.Elipse1.TargetControl = Me.Button1
+        '
+        'Elipse2
+        '
+        Me.Elipse2.BorderRadius = 100
+        Me.Elipse2.TargetControl = Me.Button2
+        '
+        'Elipse3
+        '
+        Me.Elipse3.BorderRadius = 100
+        Me.Elipse3.TargetControl = Me.Button3
+        '
+        'Elipse4
+        '
+        Me.Elipse4.BorderRadius = 100
+        Me.Elipse4.TargetControl = Me.Button4
+        '
+        'Elipse5
+        '
+        Me.Elipse5.BorderRadius = 100
+        Me.Elipse5.TargetControl = Me.GroupBox1
+        '
+        'Elipse6
+        '
+        Me.Elipse6.BorderRadius = 100
+        Me.Elipse6.TargetControl = Me.Button5
+        '
+        'ibrahimcelik
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(2679, 1537)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ChromiumWebBrowser1)
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.Name = "ibrahimcelik"
+        Me.Text = "SoftwareDEVibrahimcelik"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ChromiumWebBrowser1 As CefSharp.WinForms.ChromiumWebBrowser
+    Friend WithEvents GroupBox1 As Guna.UI2.WinForms.Guna2GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ElipsMain As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents DragControl1 As Guna.UI2.WinForms.Guna2DragControl
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Elipse2 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Elipse3 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Elipse4 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Elipse5 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Elipse6 As Guna.UI2.WinForms.Guna2Elipse
+End Class
---
+++ ibrahimcelik - Game Collection for Steam/IBRAHIM CELIK - Game Collection for Steam/ibrahimcelik.vb
@@ -0,0 +1,143 @@
+﻿Imports CefSharp
+Imports Microsoft.Win32
+Imports System
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+Imports System.Net
+Imports System.IO
+Imports System.Text
+Imports System.Drawing.Imaging
+Imports System.Text.RegularExpressions
+Imports CefSharp.WinForms.CefSettings
+Imports CefSharp.WinForms
+Imports System.Runtime.InteropServices
+Imports CefSharp.DevTools
+Imports CefSharp.DevTools.Page
+Imports System.Runtime.InteropServices.ExternalException
+
+Public Class ibrahimcelik
+
+    'yo19
+    'Reedited & updated for 64 bit by software developer IBRAHIM CELIK
+    'YouTube: @devibrahimcelik | : https://www.youtube.com/@devibrahimcelik
+    'Github: https://github.com/SoftwareDEVibrahimcelik
+    'Instagram: softwaredevic
+    'Donate Bitcoin Address: 3H2iUqWmQ2RGTYDjJwceVeEFT8XMTafjrk
+
+
+
+    Private Sub ibrahimcelik_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Cef.GetGlobalCookieManager().DeleteCookies("", "")
+
+        Me.Size = New Size(2705, 1608)
+
+
+    End Sub
+
+    Private WithEvents browser As ChromiumWebBrowser
+
+
+
+    Public Sub New()
+        InitializeComponent()
+        Dim settings As New CefSettings()
+        '
+        settings.CefCommandLineArgs.Add("disable-3rd-party-cookies", "0")
+        settings.CefCommandLineArgs.Add("disable-image-loading", "1")
+        settings.CefCommandLineArgs.Add("mute-audio")
+
+
+
+
+        '
+        CefSharp.Cef.Initialize(settings)
+
+        browser = New ChromiumWebBrowser("https://store.steampowered.com/login/") With {
+            .Dock = DockStyle.Fill
+        }
+        ChromiumWebBrowser1.Controls.Add(browser)
+        browser.Dock = DockStyle.Fill
+        Timer1.Start()
+        AddHandler browser.FrameLoadEnd, AddressOf FrameLoaded
+    End Sub
+
+
+
+    Private Sub FrameLoaded(sender As Object, e As FrameLoadEndEventArgs)
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        'You need to get 'addtocart' button id from steam game pages...
+        browser.ExecuteScriptAsync("addToCart(45549); addToCart(74002); addToCart(213185); addToCart(111113); addToCart(117458); addToCart(96994); addToCart(96994); addToCart(165867); addToCart(96994); addToCart(189289); addToCart(195824); addToCart(249023); addToCart(354552); addToCart(371737); addToCart(371819); addToCart(391764); addToCart(399411); addToCart(410041); addToCart(419480); addToCart(543624); addToCart(552512); addToCart(85819); addToCart(386888); addToCart(212715); addToCart(387773); addToCart(38690); addToCart(144399); addToCart(35999); addToCart(52512); addToCart(143197); addToCart(484025); addToCart(92277); addToCart(306628); addToCart(44117); addToCart(65178); addToCart(95807); addToCart(552965); addToCart(577461); addToCart(1596); addToCart(30867); addToCart(76056); addToCart(115596); addToCart(382481); addToCart(44120); addToCart(475059); addToCart(388041); addToCart(413333); addToCart(121); addToCart(126); addToCart(127); addToCart(123); addToCart(124); addToCart(265); addToCart(1051); addToCart(2079); addToCart(6217); addToCart(2258); addToCart(6812); addToCart(6615); addToCart(18532); addToCart(15714); addToCart(15376); addToCart(30888); addToCart(31560); addToCart(32275); addToCart(49817); addToCart(51708); addToCart(59898); addToCart(84542); addToCart(85370); addToCart(91104); addToCart(92315); addToCart(94528); addToCart(97318); addToCart(101346); addToCart(109772); addToCart(116012); addToCart(119798); addToCart(147478); addToCart(151272); addToCart(488914); addToCart(498269); addToCart(515851); addToCart(995876); addToCart(995870); addToCart(1041477); addToCart(998005); addToCart(1006563); addToCart(402134);")
+
+    End Sub
+
+
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        browser.ExecuteScriptAsync("
+            const removeButtons = document.querySelectorAll('div._33j4SwfO2YH9eI6qV9BKaL.Panel.Focusable'); // Kaldırma düğmelerini seç
+            // Her bir kaldırma düğmesine tıkla
+            removeButtons.forEach(removeButton => {
+                removeButton.click(); // Kaldırma düğmesine tıkla
+            });
+        ")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim script As String = "
+            const gameDivs = document.querySelectorAll('div.jRg6Oo9hDgKCGaXi9NJvU');
+            gameDivs.forEach(gameDiv => {
+                const ownedMessage = gameDiv.querySelector('div._1PZ2hvQoEPUrRR-9jaAMBK');
+                if (ownedMessage) {
+                    const removeButton = gameDiv.querySelector('div._33j4SwfO2YH9eI6qV9BKaL.Panel.Focusable');
+                    if (removeButton) {
+                        removeButton.click();
+                    }
+                }
+            });
+        "
+
+        browser.ExecuteScriptAsync(script)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        browser.ExecuteScriptAsync("
+            (function() {
+                const gameDivs = document.querySelectorAll('div.jRg6Oo9hDgKCGaXi9NJvU');
+                gameDivs.forEach(function(gameDiv) {
+                    const discountElement = gameDiv.querySelector('span.StoreSaleDiscountBox');
+                    if (!discountElement) {
+                        const removeButton = gameDiv.querySelector('div._33j4SwfO2YH9eI6qV9BKaL.Panel.Focusable');
+                        if (removeButton) {
+                            removeButton.click();
+                        }
+                    }
+                });
+            })()")
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Clipboard.SetDataObject("3H2iUqWmQ2RGTYDjJwceVeEFT8XMTafjrk")
+        MsgBox("YOU COPIED IBRAHIM CELIK IS BITCOIN ADDRESS")
+
+        System.Diagnostics.Process.Start("https://www.youtube.com/channel/@devibrahimcelik")
+
+    End Sub
+End Class
